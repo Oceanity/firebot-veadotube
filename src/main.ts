ced3c1145770e477b40f45d2aa06af71ce8e708f
Oceanity <github@oceanity.net>
2025-08-17 14:27:01 -0700

    Moved packageJson values to constants, beginning of new client/integration usage

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,59 @@
-import { initModules } from "@oceanity/firebot-helpers/firebot";
-import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
+import {
+  Firebot,
+  Integration,
+} from "@crowbartools/firebot-custom-scripts-types";
+import {
+  initModules,
+  integrationManager,
+} from "@oceanity/firebot-helpers/firebot";
 import * as packageJson from "../package.json";
-import { VeadtoubeService } from "./veadotube";
-import { AllVeadotubeEffects } from "./veadotube/effects";
+import {
+  VEADOTUBE_EVENT_SOURCE,
+  VEADOTUBE_INTEGRATION_AUTHOR,
+  VEADOTUBE_INTEGRATION_DEFINITION,
+  VEADOTUBE_INTEGRATION_DESCRIPTION,
+  VEADOTUBE_INTEGRATION_FIREBOT_VERSION,
+  VEADOTUBE_INTEGRATION_NAME_WITH_AUTHOR,
+  VEADOTUBE_INTEGRATION_VERSION,
+} from "./constants";
+import { AllVeadotubeEffects } from "./effects";
+import { VeadotubeIntegrationSettings } from "./types";
+import { AllVeadotubeVariables } from "./variables";
+import { VeadotubeService } from "./veadotube";
+import { initVeadotubeIntegration } from "./veadotube-integration";
 import { setupFrontendListeners } from "./veadotube/communicator";
-import { initRemote } from "./veadotube/veadotube-remote";
-import { VeadotubeEventSource } from "./veadotube/events/veadotube-event-source";
-import { AllVeadotubeVariables } from "./veadotube/variables";
 
 export const { displayName: name, description, version, author } = packageJson;
 
-export let veadotube: VeadtoubeService;
+export let veadotube: VeadotubeService;
 
-interface Params {
-  veadotubeServer: string;
-  veadotubeInstanceType: string;
-}
-
-const script: Firebot.CustomScript<Params> = {
+const script: Firebot.CustomScript = {
   getScriptManifest: () => {
     return {
-      name,
-      description,
-      author,
-      version,
-      firebotVersion: "5",
+      name: VEADOTUBE_INTEGRATION_NAME_WITH_AUTHOR,
+      description: VEADOTUBE_INTEGRATION_DESCRIPTION,
+      author: VEADOTUBE_INTEGRATION_AUTHOR,
+      version: VEADOTUBE_INTEGRATION_VERSION,
+      firebotVersion: VEADOTUBE_INTEGRATION_FIREBOT_VERSION,
     };
   },
-  getDefaultParameters: () => {
-    return {
-      veadotubeServer: {
-        type: "string",
-        default: "127.0.0.1:<port>",
-        description: "Veadotube Server Address",
-        secondaryDescription: "Enter your server IP address and port, be sure to set it in manually in `program settings` in Veadotube or the port will change every time you restart Veadotub",
-      },
-      veadotubeInstanceType: {
-        type: "string",
-        default: "mini",
-        description: "Veadotube Instance Type",
-        secondaryDescription: "Input `mini` for Veadotube Mini, `live` for Veadotube Live, or `editor` for Veadotube Editor",
-      }
-    };
-  },
+  getDefaultParameters: () => ({}),
   run: (runRequest) => {
-    const { modules, parameters } = runRequest;
-
-    if (!parameters.veadotubeServer) {
-      throw new Error("Veadotube Server Address not set");
-    }
-
-    if (!parameters.veadotubeInstanceType) {
-      throw new Error("Veadotube Instance Type not set");
-    }
-
-    if (["mini", "live", "editor"].indexOf(parameters.veadotubeInstanceType) === -1) {
-      throw new Error("Veadotube Instance Type not valid");
-    }
+    const { modules } = runRequest;
 
     initModules(modules);
-    initRemote(parameters.veadotubeServer, parameters.veadotubeInstanceType as VeadotubeInstanceType);
+
+    const integration: Integration<VeadotubeIntegrationSettings> = {
+      definition: VEADOTUBE_INTEGRATION_DEFINITION,
+      integration: initVeadotubeIntegration(),
+    };
+
+    integrationManager.registerIntegration(integration);
+
+    // initRemote(
+    //   parameters.veadotubeServer,
+    //   parameters.veadotubeInstanceType as VeadotubeInstanceType
+    // );
 
     // Register Communicator
     setupFrontendListeners(modules.frontendCommunicator);
@@ -70,7 +64,7 @@ const script: Firebot.CustomScript<Params> = {
     }
 
     // Register Events
-    modules.eventManager.registerEventSource(VeadotubeEventSource);
+    modules.eventManager.registerEventSource(VEADOTUBE_EVENT_SOURCE);
 
     // Register Variables
     for (const variable of AllVeadotubeVariables) {
